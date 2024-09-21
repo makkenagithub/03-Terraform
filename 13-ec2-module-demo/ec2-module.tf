56457c9a7f2fb37914c35fcb94cc2e95483b9f73
makkenagithub <suresh.makkena@gmail.com>
2024-09-21 10:53:41 +0530

    modules demo

--- 13-ec2-module-demo/ec2-module.tf
+++ 13-ec2-module-demo/ec2-module.tf
@@ -1,4 +1,9 @@
 module "ec2" {
     source = "../03-terraform-aws-ec2" 
+    #these (ami_id) are variables expected by module, not the arguments of resource definitions
+    # we can also cretae our own variables and pass to modules or else pass direct values.
+    ami_id = var.ami_something
+    instance_type = "t3.small"
+    sg_id = ["sg-010112", "sg-00001000"]
 
 }
---
+++ 13-ec2-module-demo/variables.tf
@@ -0,0 +1,4 @@
+variable "ami_something" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
